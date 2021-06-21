dde8f4e80d9d15daf470eb8b8168ad3cfb7e4932
Kiernan2 <kiernan1994@gmail.com>
2021-06-21 14:04:59 -0700

    Added click highlight

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  
+  $("#p1").click(function() {
+    $("#p1").addClass("clicked")
+  });
+
+  $("#p2").click(function() {
+    $("#p2").addClass("clicked")
+  });
+
+  $("#p3").click(function() {
+    $("#p3").addClass("clicked")
+  });
+
+});
